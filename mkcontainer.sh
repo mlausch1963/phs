9a8049c777d58e695f17cdeb65a6674125b79cba
Michael Lausch <mla@lausch.at>
2020-09-05 23:49:31 +0200

    Add containerbuild shell script.

---
+++ mkcontainer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+tag=$(git rev-parse --short HEAD)
+containerid=$(buildah from debian:buster)
+buildah copy ${containerid} bin/phs /usr/local/bin/phs
+buildah config --cmd /usr/local/bin/phs ${containerid}
+buildah commit ${containerid} registry.home.lausch.at/k8s-experiments/webapp1/phs:${tag}
+buildah commit ${containerid} registry.home.lausch.at/k8s-experiments/webapp1/phs:latest
+buildah images
+podman push registry.home.lausch.at/k8s-experiments/webapp1/phs:${tag}
+podman push registry.home.lausch.at/k8s-experiments/webapp1/phs:latest
